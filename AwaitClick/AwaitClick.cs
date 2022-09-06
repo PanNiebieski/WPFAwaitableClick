5c992f48e363d48f593effb028d5263f223247a6
Cezary Walenciuk <cezarywalenciuk@gmail.com>
2022-09-06 08:20:29 +0200

    Add project files.

---
+++ AwaitClick/AwaitClick.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AwaitClick
+{
+public static class  StaticExtensionAwaitClick
+{
+    public static Task WhenClicked(this Button btn)
+    {
+        var tcs = new TaskCompletionSource<object>();
+        RoutedEventHandler lambda = null;
+
+        lambda = (s, e) =>
+        {
+            btn.Click -= lambda;
+            tcs.SetResult(null);
+        };
+
+        btn.Click += lambda;
+        return tcs.Task;
+    }
+
+public static Task WhenClicked(this Button btn,Action action)
+{
+    var tcs = new TaskCompletionSource<object>();
+    RoutedEventHandler lambda = null;
+
+    lambda = (s, e) =>
+    {
+
+        action.Invoke();
+        tcs.SetResult(null);
+    };
+
+    btn.Click += lambda;
+    return tcs.Task;
+}
+}
+}
---
+++ AwaitClick/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+
+namespace AwaitClick
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+public partial class MainWindow : Window
+{
+public MainWindow()
+{
+    InitializeComponent();
+    btnTest.WhenClicked().ContinueWith(k => { MessageBox.Show("Po klinieciu"); });
+    StopAfterButton();
+
+//działa tylko raz
+btnNoClickEvent.WhenClicked( 
+    () => 
+    MessageBox.Show("Działa tylko raz"));
+}
+
+public async Task StopAfterButton()
+{
+    tb_wait.Text = "Nacisnij przycisk aby przejść dalej ";
+    await btnTest.WhenClicked();
+    tb_wait.Text = "Dzięki";
+}
+
+    private void btnTest_Click(object sender, RoutedEventArgs e)
+    {
+        btnTest.WhenClicked().ContinueWith(k => { MessageBox.Show("Po klinieciu"); });
+        MessageBox.Show("W trakcie kliknięcia");
+   
+    }
+
+
+
+}
+}
